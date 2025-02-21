a52b0d6f15c067ea557c806bb3ebc9adedcc3880
MichaelReeves2 <tadeas.jindra28@seznam.cz>
2025-02-21 12:09:08 +0100

    basic GUI

--- src/main/java/com/javafx/Controller.java
+++ src/main/java/com/javafx/Controller.java
@@ -1,7 +1,26 @@
 package com.javafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
+    @FXML
+    private TextField input;
 
+    @FXML   
+    private TextField output;
+
+    @FXML
+    private TextField iterations;
+
+    @FXML
+    private TextField steps;
+
+
+    public void run() {
+        String inputValue = input.getText();
+
+        
+    }
 }
